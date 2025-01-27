5060178cef646f411467b96c61d6a7f1d2fac1c9
Kamila <77589791+kondratkamila@users.noreply.github.com>
2025-01-27 21:07:20 +0100

    Update ZarzadzanieZadaniami.sql

--- ZarzadzanieZadaniami.sql
+++ ZarzadzanieZadaniami.sql
@@ -301,22 +301,6 @@ END;
 
 ---------------------------------------------------
 
-
-CREATE VIEW UserTasks AS
-SELECT t.TaskId, t.Title, t.Description, t.Status, t.Priority, t.OwnerId
-FROM Tasks t
-JOIN Users u ON t.OwnerId = u.UserId
-LEFT JOIN Permissions p ON t.TaskId = p.TaskId
-WHERE
-    -- Menedżerzy mają dostęp do wszystkich zadań
-    (u.UserId = USER_ID() AND EXISTS (SELECT 1 FROM Roles r WHERE r.UserId = u.UserId AND r.Role = 'Manager'))
-    -- Zwykli użytkownicy widzą tylko swoje zadania lub zadania udostępnione im
-    OR t.OwnerId = USER_ID() 
-    OR p.SharedWith = USER_ID();
-
-
---------------------------------------------------
-
 -- Procedura: Raport dla podmiotów
 CREATE PROCEDURE TenantActivityReport
     @TenantId INT
@@ -369,3 +353,15 @@ EXEC ArchiveOldTasks @CutoffDate;
 SELECT * FROM ArchivedTasks;
 -- Dane pozostałe w tabeli Tasks
 SELECT * FROM Tasks;
+
+--CREATE VIEW UserTasks AS
+--SELECT t.TaskId, t.Title, t.Description, t.Status, t.Priority, t.OwnerId
+--FROM Tasks t
+--JOIN Users u ON t.OwnerId = u.UserId
+--LEFT JOIN Permissions p ON t.TaskId = p.TaskId
+--WHERE
+    -- Menedżerzy mają dostęp do wszystkich zadań
+    --(u.UserId = USER_ID() AND EXISTS (SELECT 1 FROM Roles r WHERE r.UserId = u.UserId AND r.Role = 'Manager'))
+    -- Zwykli użytkownicy widzą tylko swoje zadania lub zadania udostępnione im
+    --OR t.OwnerId = USER_ID() 
+    --OR p.SharedWith = USER_ID();
